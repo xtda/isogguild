2a09f8d182c65c3d389f9df62e357fc564edf937
Andrew Jackson <andrew@disvelop.net>
2018-02-09 22:16:43 +1100

    simple project setup

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,15 @@ module Isogguild
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
+
+    config.time_zone = 'Australia/Melbourne'
+
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
